167dac246f3dfbcdd64a20576e8a9349e9b8e3cb
Wonhee <totohwh3601@naver.com>
2024-06-30 01:13:40 +0900

    [Feat] 프로젝트 초기설정

---
+++ src/main/java/com/healo/HealoBeApplication.java
@@ -0,0 +1,13 @@
+package com.healo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealoBeApplication.class, args);
+    }
+
+}
