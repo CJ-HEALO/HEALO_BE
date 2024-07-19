23245da8f6a3cf125fbba2de71de1d21ddaf1e8e
Wonhee <totohwh3601@naver.com>
2024-07-19 14:34:02 +0900

    [Feat] Enum 타입 수정

--- src/main/java/com/healo/domain/base/entity/Status.java
+++ src/main/java/com/healo/domain/base/entity/Status.java
@@ -1,6 +1,16 @@
 package com.healo.domain.base.entity;
 
+import lombok.Getter;
+
 public enum Status {
-    ACTIVE,
-    INACTIVE
+    ACTIVE("활성"),
+    INACTIVE("비활성");
+
+    private final String value;
+    Status(String state) {
+        this.value = state;
+    }
+    public String getValue(){
+        return value;
+    }
 }
--- src/main/java/com/healo/domain/user/entity/RoleType.java
+++ src/main/java/com/healo/domain/user/entity/RoleType.java
@@ -1,6 +1,10 @@
 package com.healo.domain.user.entity;
 
 public enum RoleType {
-    USER,
-    ADMIN
+    USER("유저"),
+    ADMIN("관리자");
+    private final String value;
+    RoleType(String value) {this.value = value;}
+    public String getValue() {return value;}
+
 }
